35f0cb0294304eaee16f12e8e59e561c08babfcb
tranthanhhieuthao <tranthanhhieuthao@gmail.com>
2021-11-24 16:56:39 +0700

    logout

--- src/layout/navbar/index.vue
+++ src/layout/navbar/index.vue
@@ -24,24 +24,34 @@
 
       <v-spacer></v-spacer>
 
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
+      <v-btn icon @click="logout()">
+        <v-icon>mdi-logout</v-icon>
       </v-btn>
 
       <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
+        <v-icon>mdi-email</v-icon>
       </v-btn>
 
-      <v-btn icon>
+      <!-- <v-btn icon>
         <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
+      </v-btn> -->
     </v-app-bar>
   </div>
 </template>
 
 <script>
     export default {
-        
+        data() {
+          return {
+            key: 0
+          }
+        },
+        methods: {
+          logout() {
+            sessionStorage.clear()
+            this.$router.replace('/')
+          }
+        },
     }
 </script>
 
