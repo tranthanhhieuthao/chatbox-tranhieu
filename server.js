5f9904613d626f4440eaf2244fd22dace9a2d72d
tranthanhhieuthao <tranthanhhieuthao@gmail.com>
2021-11-25 19:13:47 +0700

    fix reload page error heroku

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const path = require('path')
 const app = express()
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'))
-})
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'))
+// })
 
 const port = process.env.PORT || 8080
 app.listen(port)
